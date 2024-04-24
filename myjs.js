19e1bf5f00f5894fbca893accfbe6b68da721187
erikabelanich <161361629+erikabelanich@users.noreply.github.com>
2024-04-23 20:05:59 -0700

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  "use strict";
+
+  var carousels = function() {
+    $(".owl-carousel1").owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false
+        },
+        1000: {
+          items: 3,
+          nav: true
+        }
+      }
+    });
+  };
+
+  carousels(); // Call the function directly inside document ready
+});
